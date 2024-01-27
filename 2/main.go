e3c50cb47bd6024cac07c1b121b60e12dfe4c7a4
Gustavo <coartem2@proton.me>
2024-01-26 22:09:52 -0300

    Update main.go

--- 2/main.go
+++ 2/main.go
@@ -3,12 +3,14 @@ package main
 const a = "hello friend"
 
 var (
-	b bool
-	c int
-	d string
-	e float64
+	b bool    = true
+	c int     = 10
+	d string  = "hello friend"
+	e float64 = 1.2
 )
 
 func main() {
-	println(e)
+	a := "x" //string
+	a = "mr"
+	println(a)
 }
