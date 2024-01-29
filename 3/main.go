71ba8ef3093f8a4db3e70da6c3b07f209bc26a00
Gustavo <coartem2@proton.me>
2024-01-29 12:55:21 -0300

    criaçao de tipos

---
+++ 3/main.go
@@ -0,0 +1,17 @@
+package main
+
+const a = "hello friend"
+
+type ID int
+
+var (
+	b bool    = true
+	c int     = 10
+	d string  = "hello friend"
+	e float64 = 1.2
+	f ID      = 1
+)
+
+func main() {
+	println(f)
+}
