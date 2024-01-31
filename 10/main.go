68037c37cae18924faf85c2b5dc5c05b12201279
Gustavo <coartem2@proton.me>
2024-01-30 22:19:03 -0300

    closures

---
+++ 10/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	total := func() int {
+		return sum(1, 2, 3, 4, 5, 8, 9, 865, 6, 87, 748, 6541, 5496) * 2
+	}()
+	fmt.Println(total)
+}
+
+func sum(numeros ...int) int {
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
+}
