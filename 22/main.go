bfc972ea6813df1e49c2530bd3ef3873778e45b9
Gustavo <coartem2@proton.me>
2024-02-04 18:43:06 -0300

    for no go
    looping infinito no final
    
    ctrl + k + c pra comentar bloco de codigo
    ctrl + k + u pra descomentar....kkkkk

--- 22/main.go
+++ 22/main.go
@@ -5,9 +5,19 @@ func main() {
 	//		println(i)
 	//	}
 
-	numeros := []string{"um", "dois", "tres"}
+	// numeros := []string{"um", "dois", "tres"}
 
-	for k, v := range numeros {
-		println(k, v)
+	// for k, v := range numeros {
+	// 	println(k, v)
+	// }
+
+	// i := 0
+	// for i < 10 {
+	// 	println(i)
+	// 	i++
+	// }
+
+	for {
+		println("hello friend")
 	}
 }
