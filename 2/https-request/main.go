9d31a55049a46c386eea5e5d30423c853ae95bb3
Gustavo <coartem2@proton.me>
2024-02-04 20:56:28 -0300

    requisiçao https

--- 2/https-request/main.go
+++ 2/https-request/main.go
@@ -1,11 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+)
 
 func main() {
-
-	req, err := http.Get("http:/google.com")
+	req, err := http.Get("https://www.google.com")
 	if err != nil {
 		panic(err)
 	}
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(res))
+	req.Body.Close()
 }
