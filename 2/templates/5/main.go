e2ac28179ad482bc23834c61581d2c546a2e20c8
Gustavo <coartem2@proton.me>
2024-02-05 17:31:40 -0300

    compondo templates

---
+++ 2/templates/5/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	templates := []string{
+		"header.html",
+		"content.html",
+		"footer.html",
+	}
+
+	t := template.Must(template.New("content.html").ParseFiles(templates...))
+	err := t.Execute(os.Stdout, Cursos{
+		{"go", 40},
+		{"java", 20},
+		{"python", 20},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
