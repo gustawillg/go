39e26a2026264f17ec19ba39cd4a3541a2a1076b
Gustavo <coartem2@proton.me>
2024-02-05 13:21:33 -0300

    criando função pro BuscaCEP

--- 2/busca-cep/main.go
+++ 2/busca-cep/main.go
@@ -1,6 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 	http.HandleFunc("/", BuscaCEPHandler)
@@ -17,7 +34,26 @@ func BuscaCEPHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("hello friend"))
+}
+
+func buscaCep(cep string) (*ViaCEP, error) {
+	resp, error := http.Get("http://viacep.com.br/ws/" + cep + "/json/")
+	if error != nil {
+		return nil, error
+	}
+	defer resp.Body.Close()
+	body, error := io.ReadAll(resp.Body)
+	if error != nil {
+		return nil, error
+	}
+	var c ViaCEP
+	error = json.Unmarshal(body, &c)
+	if error != nil {
+		return nil, error
+	}
+	return &c, nil
 }
