de2e89b71b6330bb30606b135ad9d192a04cefd0
Gustavo <coartem2@proton.me>
2024-02-12 19:55:43 -0300

    a

--- 5/4/main.go
+++ 5/4/main.go
@@ -8,17 +8,17 @@ import (
 )
 
 type Category struct {
-	ID       int `gorm:"primaryKey"`
-	Name     string
-	Products []Product
+	ID   int `gorm:"primaryKey"`
+	Name string
 }
 
 type Product struct {
-	ID         int `gorm:"primaryKey"`
-	Name       string
-	Price      float64
-	CategoryID int
-	Category   Category
+	ID           int `gorm:"primaryKey"`
+	Name         string
+	Price        float64
+	CategoryID   int
+	Category     Category
+	SerialNumber SerialNumber
 	gorm.Model
 }
 
@@ -29,39 +29,33 @@ type SerialNumber struct {
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4parseTime=True&loc=cdLocal"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&Product{}, &Category{})
+	db.AutoMigrate(&Product{}, &Category{}, &SerialNumber{})
 
-	//create category
-	category := Category{Name: "cozinha"}
+	// create category
+	category := Category{Name: "Eletronicos"}
 	db.Create(&category)
 
-	//create product
+	// create product
 	db.Create(&Product{
-		Name:       "panela",
-		Price:      10.00,
-		CategoryID: 2,
+		Name:       "Mouse",
+		Price:      1000.00,
+		CategoryID: 1,
 	})
 
-	//create serial number
+	// create serial number
 	db.Create(&SerialNumber{
 		Number:    "123456",
 		ProductID: 1,
 	})
 
-	var categories []Category
-	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error
-	if err != nil {
-		panic(err)
-	}
-	for _, category := range categories {
-		fmt.Println(category.Name, ":")
-		for _, product := range category.Products {
-			println("- ", product.Name, category.Name)
-		}
+	var products []Product
+	db.Preload("Category").Preload("SerialNumber").Find(&products)
+	for _, product := range products {
+		fmt.Println(product.Name, product.Category.Name, product.SerialNumber.Number)
 	}
 }
