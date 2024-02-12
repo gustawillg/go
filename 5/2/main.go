b55e577303216577694c236effcb36ab6e4f365b
Gustavo <coartem2@proton.me>
2024-02-12 13:56:02 -0300

    configurando e criando operações

---
+++ 5/2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	ID    int `gorm:"primaryKey"`
+	Name  string
+	Price float64
+}
+
+func main() {
+	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Product{})
+
+	//create
+	// db.Create(&Product{
+	// 	Name:  "test",
+	// 	Price: 1000.00,
+	// })
+
+	//create batch
+	products := []Product{
+		{Name: "test", Price: 1000}
+		{Name: "test2", Price: 13000}
+		{Name: "test3", Price: 41000}
+	}
+	db.Create(&products)
+}
