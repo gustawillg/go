2d660ad7de735c7ac650737f68bd79b9e6a1705b
Gustavo <coartem2@proton.me>
2024-02-12 22:16:19 -0300

    teste

--- 6/1/cmd/main.go
+++ 6/1/cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gustawillg/go/tree/main/6/1/math"
+)
 
 func main() {
-	fmt.Println("hello friend")
+	n := math.Math(A: 1, B:2)
+	fmt.Println(M.Add())
+	// fmt.Println("hello friend")
 }
