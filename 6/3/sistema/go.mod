92a3092184e9c7106b5cb38c16cd43c4269b3572
Gustavo <coartem2@proton.me>
2024-02-12 23:28:58 -0300

    trabalhando com go mod replace

---
+++ 6/3/sistema/go.mod
@@ -0,0 +1,3 @@
+module github.com/gustawillg/go/tree/main/6/3/sistem
+
+go 1.21.6
---
+++ 6/3/sistema/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/gustawillg/go/tree/main/6/1/math"
+
+func main() {
+	m := math.NewMath(1, 2)
+	println(m.Add())
+}
