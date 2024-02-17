7d39142d0962597cf7e5a3190c9a47c638bd12e6
Gustavo <coartem2@proton.me>
2024-02-16 22:32:11 -0300

    ajustando package para os handlers

---
+++ 9/internal/webserver/handlers/product_handlers.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gustawillg/go/9/configs"
+	"github.com/gustawillg/go/9/internal/entity"
+	"github.com/gustawillg/go/9/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
+}
